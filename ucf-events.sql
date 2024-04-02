505463fbbdcefd93761a3c22b0be07991841aab1
Joshua Bartz <159960300+Ashenryn@users.noreply.github.com>
2024-04-02 06:41:24 -0400

    Updated model
    incomplete. added users with default values to test with creating a login system for the webpage. passwords are 'password', names are default_user, default_user2, etc.

--- ucf-events.sql
+++ ucf-events.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Mar 31, 2024 at 05:34 PM
+-- Generation Time: Apr 02, 2024 at 10:40 AM
 -- Server version: 8.2.0
 -- PHP Version: 8.2.13
 
@@ -31,9 +31,17 @@ DROP TABLE IF EXISTS `admin`;
 CREATE TABLE IF NOT EXISTS `admin` (
   `UID` int NOT NULL,
   `Admins_ID` int NOT NULL,
+  `Admins_name` text,
   PRIMARY KEY (`UID`),
   KEY `Admins_ID` (`Admins_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`UID`, `Admins_ID`, `Admins_name`) VALUES
+(2, 0, 'default_admin');
 
 -- --------------------------------------------------------
 
@@ -72,6 +80,22 @@ CREATE TABLE IF NOT EXISTS `location` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `private_creates`
+--
+
+DROP TABLE IF EXISTS `private_creates`;
+CREATE TABLE IF NOT EXISTS `private_creates` (
+  `Events_ID` int NOT NULL,
+  `Admins_ID` int NOT NULL,
+  `SuperAdmins_ID` int NOT NULL,
+  KEY `priv_admin_creates` (`Admins_ID`),
+  KEY `priv_sadmin_creates` (`SuperAdmins_ID`),
+  KEY `priv_makes_event` (`Events_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `private_events`
 --
 
@@ -81,8 +105,56 @@ CREATE TABLE IF NOT EXISTS `private_events` (
   `Admins_ID` int NOT NULL,
   `SuperAdmins_ID` int NOT NULL,
   PRIMARY KEY (`Events_ID`),
+  UNIQUE KEY `Admins_ID_2` (`Admins_ID`,`SuperAdmins_ID`),
   KEY `Admins_ID` (`Admins_ID`,`SuperAdmins_ID`),
   KEY `super_admin_creates` (`SuperAdmins_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `public_creates`
+--
+
+DROP TABLE IF EXISTS `public_creates`;
+CREATE TABLE IF NOT EXISTS `public_creates` (
+  `Events_ID` int NOT NULL,
+  `Admins_ID` int NOT NULL,
+  `SuperAdmins_ID` int NOT NULL,
+  PRIMARY KEY (`Events_ID`,`Admins_ID`,`SuperAdmins_ID`),
+  KEY `pub_made_by_admin` (`Admins_ID`),
+  KEY `pub_made_by_sadmin` (`SuperAdmins_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `public_events`
+--
+
+DROP TABLE IF EXISTS `public_events`;
+CREATE TABLE IF NOT EXISTS `public_events` (
+  `Events_ID` int NOT NULL,
+  `Admins_ID` int NOT NULL,
+  `SuperAdmins_ID` int NOT NULL,
+  PRIMARY KEY (`Events_ID`),
+  UNIQUE KEY `Admins_ID` (`Admins_ID`,`SuperAdmins_ID`),
+  KEY `sadmin_creates_pubE` (`SuperAdmins_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rso`
+--
+
+DROP TABLE IF EXISTS `rso`;
+CREATE TABLE IF NOT EXISTS `rso` (
+  `RSO_ID` int NOT NULL AUTO_INCREMENT,
+  `RSO_name` int NOT NULL,
+  `Admin_ID` int NOT NULL,
+  PRIMARY KEY (`RSO_ID`),
+  KEY `Admin_ID` (`Admin_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -96,6 +168,7 @@ CREATE TABLE IF NOT EXISTS `super_admin` (
   `UID` int NOT NULL,
   `SuperAdmins_ID` int NOT NULL,
   `Admins_ID` int DEFAULT NULL,
+  `SuperAdmins_name` text,
   PRIMARY KEY (`SuperAdmins_ID`),
   UNIQUE KEY `UID` (`UID`),
   KEY `Admins_ID` (`Admins_ID`)
@@ -112,8 +185,19 @@ CREATE TABLE IF NOT EXISTS `users` (
   `UID` int NOT NULL AUTO_INCREMENT,
   `pass` text,
   `name` text,
-  PRIMARY KEY (`UID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `RSO_ID` int DEFAULT NULL COMMENT 'ID of the RSO this user has joined',
+  PRIMARY KEY (`UID`),
+  KEY `RSO_ID` (`RSO_ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`UID`, `pass`, `name`, `RSO_ID`) VALUES
+(2, 'password', 'default_user', NULL),
+(3, 'password', 'default_user2', NULL),
+(4, 'password', 'default_user3', NULL);
 
 --
 -- Constraints for dumped tables
@@ -123,13 +207,13 @@ CREATE TABLE IF NOT EXISTS `users` (
 -- Constraints for table `admin`
 --
 ALTER TABLE `admin`
-  ADD CONSTRAINT `admin_ibfk_1` FOREIGN KEY (`Admins_ID`) REFERENCES `super_admin` (`Admins_ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `admin_isa_user` FOREIGN KEY (`UID`) REFERENCES `users` (`UID`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
 --
 -- Constraints for table `events`
 --
 ALTER TABLE `events`
-  ADD CONSTRAINT `events_ibfk_1` FOREIGN KEY (`Events_ID`) REFERENCES `private_events` (`Events_ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `events_ibfk_1` FOREIGN KEY (`Events_ID`) REFERENCES `private_events` (`Events_ID`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
 --
 -- Constraints for table `location`
@@ -138,23 +222,51 @@ ALTER TABLE `location`
   ADD CONSTRAINT `location_ibfk_1` FOREIGN KEY (`Lname`) REFERENCES `events` (`Lname`);
 
 --
+-- Constraints for table `private_creates`
+--
+ALTER TABLE `private_creates`
+  ADD CONSTRAINT `priv_admin_creates` FOREIGN KEY (`Admins_ID`) REFERENCES `admin` (`Admins_ID`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `priv_makes_event` FOREIGN KEY (`Events_ID`) REFERENCES `private_events` (`Events_ID`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `priv_sadmin_creates` FOREIGN KEY (`SuperAdmins_ID`) REFERENCES `super_admin` (`SuperAdmins_ID`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
 -- Constraints for table `private_events`
 --
 ALTER TABLE `private_events`
-  ADD CONSTRAINT `admin_creates` FOREIGN KEY (`Admins_ID`) REFERENCES `admin` (`Admins_ID`),
-  ADD CONSTRAINT `super_admin_creates` FOREIGN KEY (`SuperAdmins_ID`) REFERENCES `super_admin` (`SuperAdmins_ID`);
+  ADD CONSTRAINT `priv_isa_event` FOREIGN KEY (`Events_ID`) REFERENCES `events` (`Events_ID`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `public_creates`
+--
+ALTER TABLE `public_creates`
+  ADD CONSTRAINT `pub_made_by_admin` FOREIGN KEY (`Admins_ID`) REFERENCES `admin` (`Admins_ID`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `pub_made_by_sadmin` FOREIGN KEY (`SuperAdmins_ID`) REFERENCES `super_admin` (`SuperAdmins_ID`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `pub_makes_event` FOREIGN KEY (`Events_ID`) REFERENCES `public_events` (`Events_ID`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `public_events`
+--
+ALTER TABLE `public_events`
+  ADD CONSTRAINT `pub_isa_event` FOREIGN KEY (`Events_ID`) REFERENCES `events` (`Events_ID`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Constraints for table `rso`
+--
+ALTER TABLE `rso`
+  ADD CONSTRAINT `RSO_created_by` FOREIGN KEY (`Admin_ID`) REFERENCES `admin` (`Admins_ID`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
 --
 -- Constraints for table `super_admin`
 --
 ALTER TABLE `super_admin`
-  ADD CONSTRAINT `isa_user` FOREIGN KEY (`UID`) REFERENCES `users` (`UID`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `sadmin_isa_admin` FOREIGN KEY (`Admins_ID`) REFERENCES `admin` (`Admins_ID`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `sadmin_isa_user` FOREIGN KEY (`UID`) REFERENCES `users` (`UID`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
 --
 -- Constraints for table `users`
 --
 ALTER TABLE `users`
-  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`UID`) REFERENCES `admin` (`UID`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`RSO_ID`) REFERENCES `rso` (`RSO_ID`) ON DELETE SET NULL ON UPDATE SET NULL;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
