14724ec8bebb45ec02a3308f9654b56767cb6d98
EvanWrote <145154485+EvanWrote@users.noreply.github.com>
2024-04-09 15:48:03 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,29 +1,60 @@
-<?php
+ <?php
 	include 'db_connection.php';
 	$conn = OpenCon();
 	mysqli_close($conn);
 ?>
+<style>
+mheader {
+	position: relative;
+	left: 30px;
+	size: 20;
+}
+mfooter {
+	position: fixed;
+	bottom: 30px;
+}
+a {
+	color: blue;
+}
+</style>
 
 <html>
-<head>
-<title>UCF Events Project</title>
-<!-- Shared style sheet for all pages.-->
-<link rel="stylesheet" type="text/css" href="style.css"/> 
-<!-- Header content -->
-<header id="app-header"></header>
-<script async src="header.js"></script>
-</head>
-<!-- Start of index.php -->
-<body>
-
-<div class="content">
-  <p>A topnav, content and a footer.</p>
-
-  <a href="link"> Testing link</a>	
-</div>
-
-<!-- Footer content -->
-<header id="app-footer"></header>
-<script async src="footer.js"></script>
-</body>
+	<head>
+		<title>Event Manager</title>
+		<script type="text/javascript" src="js/md5.js"></script>
+		<script type="text/javascript" src="js/code.js"></script>
+		<link href="css/styles.css" rel="stylesheet" />
+		<link
+		href="https://fonts.googleapis.com/css?family=Ubuntu"
+		rel="stylesheet"
+		/>
+	</head>
+	<body>
+		<div id="logoIconAndName">
+		<h1 id="title">Event Manager</h1>
+		</div>
+		<div class="loginContainer">
+		<div id="loginDiv">
+			<span id="inner-title">Log In</span><br />
+			<label for="loginName">Email</label>
+			<input type="text" id="loginName" /><br />
+			<label for="loginPassword">Password</label>
+			<input type="password" id="loginPassword" /><br />
+			<button
+			type="button"
+			id="loginButton"
+			class="buttons"
+			onclick="doLogin();"
+			>
+			Log In
+			</button>
+			<span id="loginResult"></span>
+			<br />
+			<div>
+			<span id="loginLinkText">Need an account? </span
+			><a href="./register.html" id="loginLink">Click here</a>
+			</div>
+		</div>
+		</div>
+	</body>
 </html>
