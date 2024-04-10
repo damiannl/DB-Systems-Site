0ff44b9ae38ac9537ec4430953750f9ee1cb0a1b
Damian <damianlucarelli1@gmail.com>
2024-04-09 20:33:48 -0400

    Removed header & footer

--- footer.js
+++
@@ -1,7 +0,0 @@
-let appFooter = `
-    <div class="footer">UCF Database Project<br>
-    Authors: Joshua Bartz, Damian Lucarelli, Evan Wrote<br>
-        <a href="https://github.com/damiannl/DB-Systems-Site"> Github Page</a>
-    </div>
-`;
-document.getElementById("app-footer").innerHTML = appFooter;
--- header.js
+++
@@ -1,9 +0,0 @@
-let appHeader = `
-    <div class="topnav">
-    <h1>College Events</h1>
-    <button class="button" href="#">Link 1</button>
-    <button class="button" href="#">Link 2</button>
-    <button class="button" href="#">Link 3</button>
-    </div>
-`;
-document.getElementById("app-header").innerHTML = appHeader;
