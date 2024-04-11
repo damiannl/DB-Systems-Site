07521bac39c14f78083f46e066d2212fb35ec2eb
Joshua Bartz <159960300+Ashenryn@users.noreply.github.com>
2024-04-11 03:14:22 -0400

    added login check and admin check

--- db_actions.php
+++ db_actions.php
@@ -1,41 +1,59 @@
 <?php
 
 function addUser($password, $name) {
-$stmt = $conn->prepare("INSERT INTO users (pass, name) VALUES (?)");
-$stmt->bind_param("ss", $password, $name);
-$stmt->execute();
+	$stmt = $conn->prepare("INSERT INTO users (pass, name) VALUES (?)");
+	$stmt->bind_param("ss", $password, $name);
+	$stmt->execute();
 }
 
 function viewUsers($conn) {
-$sql = "SELECT UID,pass FROM users";
-$result = mysqli_query($conn,$sql);
-$row=mysqli_fetch_all($result,MYSQLI_ASSOC);
-$count = count($row);
-for($x=0;$x<$count;$x++){ //first index is the row of table, second index is column
-  printf("UID: %d,", $row[$x]["UID"]);
-  printf("pass: %s\n", $row[$x]["pass"]);
-  }
+	$sql = "SELECT UID,pass FROM users";
+	$result = mysqli_query($conn,$sql);
+	$row=mysqli_fetch_all($result,MYSQLI_ASSOC);
+	$count = count($row);
+	for($x=0;$x<$count;$x++){ //first index is the row of table, second index is column
+	  printf("UID: %d,", $row[$x]["UID"]);
+	  printf("pass: %s\n", $row[$x]["pass"]);
+	  }
 }
 
 function viewAdmins($conn) {
-$sql = "SELECT U.UID, A.Admins_ID FROM users U,admin A WHERE U.UID=A.UID";
-$result = mysqli_query($conn,$sql);
-$row = mysqli_fetch_all($result,MYSQLI_ASSOC);
-$count = count($row);
-for($x = 0; $x < $count; $x++){ //first index is the row of table, second index is column
-  printf("UID: %d,", $row[$x]["UID"]);
-  printf("Admins_ID: %d\n", $row[$x]["Admins_ID"]);
-  }
+	$sql = "SELECT U.UID, A.Admins_ID FROM users U,admin A WHERE U.UID=A.UID";
+	$result = mysqli_query($conn,$sql);
+	$row = mysqli_fetch_all($result,MYSQLI_ASSOC);
+	$count = count($row);
+	for($x = 0; $x < $count; $x++){ //first index is the row of table, second index is column
+	  printf("UID: %d,", $row[$x]["UID"]);
+	  printf("Admins_ID: %d\n", $row[$x]["Admins_ID"]);
+	  }
 }
 
+//Function that takes a user ID and a password and checks if there's a user that matches those credentials
 function verifyLogin($conn, $uid, $pass) {
-$sql = "SELECT * FROM users WHERE UID = ? AND pass = ?";
-$stmt = $conn->prepare($sql);
-$stmt->bind_param("ss", $uid, $pass);
-$stmt->execute();
-$result = $stmt->get_result();
-$array = mysqli_fetch_all($result, MYSQLI_ASSOC);
-printf("result is: %s", $array[0]["name"]);
+	$sql = "SELECT * FROM users WHERE UID = ? AND pass = ?";
+	$stmt = $conn->prepare($sql);
+	$stmt->bind_param("ss", $uid, $pass);
+	$stmt->execute();
+	$result = $stmt->get_result();
+	//Fetch all ***shouldnt*** be problematic here, since user IDs are unique only 1 entry should be in the array
+	$array = mysqli_fetch_all($result, MYSQLI_ASSOC);
+	//printf("result is: %s", $array[0]["name"]);
+	return $array;
 }
 
+//Function that determines if a user is an admin or not from a user ID
+function isAdmin($conn, $uid) {
+	$sql = "SELECT * FROM users U, admin A WHERE U.UID = ? AND U.UID = A.UID";
+	$stmt = $conn->prepare($sql);
+	$stmt->bind_param("s", $uid);
+	$stmt->execute();
+	$result = $stmt->get_result();
+	$array = mysqli_fetch_all($result, MYSQLI_NUM);
+	if($array==null) {
+		echo("false");
+		return false;
+	}
+	echo("true");
+	return true;
+}
 ?>
