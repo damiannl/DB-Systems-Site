dcc74cb1500f330d15242a19b036323c7a4a8071
Joshua Bartz <159960300+Ashenryn@users.noreply.github.com>
2024-04-11 02:42:37 -0400

    Added verify login functionality

--- db_actions.php
+++ db_actions.php
@@ -20,12 +20,22 @@ for($x=0;$x<$count;$x++){ //first index is the row of table, second index is col
 function viewAdmins($conn) {
 $sql = "SELECT U.UID, A.Admins_ID FROM users U,admin A WHERE U.UID=A.UID";
 $result = mysqli_query($conn,$sql);
-$row=mysqli_fetch_all($result,MYSQLI_ASSOC);
+$row = mysqli_fetch_all($result,MYSQLI_ASSOC);
 $count = count($row);
-for($x=0;$x<$count;$x++){ //first index is the row of table, second index is column
+for($x = 0; $x < $count; $x++){ //first index is the row of table, second index is column
   printf("UID: %d,", $row[$x]["UID"]);
   printf("Admins_ID: %d\n", $row[$x]["Admins_ID"]);
   }
+}
+
+function verifyLogin($conn, $uid, $pass) {
+$sql = "SELECT * FROM users WHERE UID = ? AND pass = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("ss", $uid, $pass);
+$stmt->execute();
+$result = $stmt->get_result();
+$array = mysqli_fetch_all($result, MYSQLI_ASSOC);
+printf("result is: %s", $array[0]["name"]);
 }
 
 ?>
